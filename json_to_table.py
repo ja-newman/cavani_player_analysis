8a9f45c419335e875d620b499ca553308c45521b
Jonathan Newman <ja.newman20@gmail.com>
2024-06-02 18:41:31 +0100

    simplified to 2 season stats, working conversion to table

---
+++ json_to_table.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+
+
+with open("cavani_2015_stats.json", "r") as file:
+    data = json.load(file)
+
+statistics = data["response"][0]["statistics"]
+
+df = pd.json_normalize(statistics,
+                       meta=[["team", "id"], ["team", "name"], ["team", "logo"], 
+                             ["league", "id"], ["league", "name"], ["league", "country"], ["league", "logo"], ["league", "flag"], ["league", "season"],
+                             ["games", "appearences"], ["games", "lineups"], ["games", "minutes"], ["games", "number"], ["games", "position"], ["games", "rating"], ["games", "captain"],
+                             ["substitutes", "in"], ["substitutes", "out"], ["substitutes", "bench"],
+                             ["shots", "total"], ["shots", "on"],
+                             ["goals", "total"], ["goals", "conceded"], ["goals", "assists"], ["goals", "saves"],
+                             ["passes", "total"], ["passes", "key"], ["passes", "accuracy"],
+                             ["tackles", "total"], ["tackles", "blocks"], ["tackles", "interceptions"],
+                             ["duels", "total"], ["duels", "won"],
+                             ["dribbles", "attempts"], ["dribbles", "success"], ["dribbles", "past"],
+                             ["fouls", "drawn"], ["fouls", "committed"],
+                             ["cards", "yellow"], ["cards", "yellowred"], ["cards", "red"],
+                             ["penalty", "won"], ["penalty", "committed"], ["penalty", "scored"], ["penalty", "missed"], ["penalty", "saved"]])
+
+df.columns = ["team_id", "team_name", "team_logo",
+              "league_id", "league_name", "league_country", "league_logo", "league_flag", "league_season",
+              "games_appearances", "games_lineups", "games_minutes", "games_number", "games_position", "games_rating", "games_captain",
+              "substitutes_in", "substitutes_out", "substitutes_bench",
+              "shots_total", "shots_on",
+              "goals_total", "goals_conceded", "goals_assists", "goals_saves",
+              "passes_total", "passes_key", "passes_accuracy",
+              "tackles_total", "tackles_blocks", "tackles_interceptions",
+              "duels_total", "duels_won",
+              "dribbles_attempts", "dribbles_success", "dribbles_past",
+              "fouls_drawn", "fouls_committed",
+              "cards_yellow", "cards_yellowred", "cards_red",
+              "penalty_won", "penalty_committed", "penalty_scored", "penalty_missed", "penalty_saved"]              
+
+
+
+
+df.to_csv("cavani_2015_stats.csv", sep=",", index=False, encoding="utf-8")
--- request_player_data.py
+++ request_player_data.py
@@ -1,19 +1,15 @@
 import json
 import requests
 
-# Players to find:
-# Edinson Cavani     ID: 274,   Team: 85 (PSG),               League: 61 (Ligue 1)
-# Zlatan Ibrahimovic ID: 51070, Team: 33 (Manchester United), League: 39 (Premier League)
-# Robert Lewandowski ID: 521,   Team: 157 (FC Bayern Munich), League: 78 (Bundesliga) ------> DOES NOT WORK!
-# Luis Suarez        ID: 157,   Team: 529 (FC Barcelona),     League: 140 (La Liga)
-# Sergio Aguero      ID: 642,   Team: 50 (Manchester City),   League: 39 (Premier League)
+# Edinson Cavani, ID: 274, Team: 85 (PSG), League: 61 (Ligue 1), Seasons 2014-15, 2015-16, 2016-17.
 
 url = "https://api-football-v1.p.rapidapi.com/v3/players"
 
 querystring = {
-    "id"    : "__",
-    "season": "____",
-    "team"  : "__"
+    "id"    : "274",
+    "season": "2014",
+    "team"  : "85",
+    "league": "61"
 }
 
 headers = {
@@ -24,5 +20,5 @@ headers = {
 response = requests.get(url, headers=headers, params=querystring)
 
 # Send response to .json file.
-with open("____stats.json", "w") as file:
+with open("cavani_2014_stats.json", "w") as file:
     json.dump(response.json(), file, indent=4)
